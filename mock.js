a9bdb1b43225266ea2d2d781b7f4d49be1e788b8
doudou <kdfshnop@163.com>
2019-03-21 15:25:48 +0800

    mock

---
+++ mock.js
@@ -0,0 +1,145 @@
+let apiData = {
+  "message": "成功",
+  "status": 1,
+  "data": {
+      "categoryList" : [
+          {
+              "categoryId": 84,
+              "title": "原味",
+              "imageUrl": "https://img.wkzf.com/140643b2e7904a02a9330916fb0b5a2f",
+              "firstSubTitleList": null
+          },
+          {
+              "categoryId": 49,
+              "title": "头条",
+              "imageUrl": "https://img.wkzf.com/ad869061671345c0a80ebcf1296f7000",
+              "firstSubTitleList": null
+          },
+          {
+              "categoryId": 73,
+              "title": "图说",
+              "imageUrl": "https://img.wkzf.com/5c29b8eb390245899b1713381918cc71",
+              "firstSubTitleList": null
+          },
+          {
+              "categoryId": 12,
+              "title": "好房",
+              "imageUrl": "https://img.wkzf.com/cdc1d36a3c5a4f3ab06c2fd65c77ffee",
+              "firstSubTitleList": [
+                  {
+                      "id": 29,
+                      "title": "新房",
+                      "secondSubTitleList": null
+                  },
+                  {
+                      "id": 30,
+                      "title": "二手房",
+                      "secondSubTitleList": null
+                  }
+              ]
+          },
+          {
+              "categoryId": 48,
+              "title": "百科",
+              "imageUrl": "https://img.wkzf.com/950e15791dd142a5b4a5d0335e183fef",
+              "firstSubTitleList": [
+                  {
+                      "id": 5,
+                      "title": "买房知识",
+                      "secondSubTitleList": [
+                          {
+                              "id": 6,
+                              "title": "贷款办理"
+                          },
+                          {
+                              "id": 7,
+                              "title": "缴税过户"
+                          },
+                          {
+                              "id": 8,
+                              "title": "看房选房"
+                          }
+                      ]
+                  },
+                  {
+                      "id": 9,
+                      "title": "卖房知识",
+                      "secondSubTitleList": [
+                          {
+                              "id": 10,
+                              "title": "房源检核"
+                          },
+                          {
+                              "id": 11,
+                              "title": "缴税过户"
+                          },
+                          {
+                              "id": 12,
+                              "title": "签订合同"
+                          },
+                          {
+                              "id": 14,
+                              "title": "物业交接"
+                          }
+                      ]
+                  },
+                  {
+                      "id": 15,
+                      "title": "租房知识",
+                      "secondSubTitleList": [
+                          {
+                              "id": 16,
+                              "title": "租房准备"
+                          },
+                          {
+                              "id": 17,
+                              "title": "租房签约"
+                          }
+                      ]
+                  },
+                  {
+                      "id": 19,
+                      "title": "装修知识",
+                      "secondSubTitleList": [
+                          {
+                              "id": 20,
+                              "title": "家具知识"
+                          },
+                          {
+                              "id": 21,
+                              "title": "竣工验收"
+                          },
+                          {
+                              "id": 22,
+                              "title": "装修施工"
+                          }
+                      ]
+                  },
+                  {
+                      "id": 31,
+                      "title": "案例说法",
+                      "secondSubTitleList": null
+                  },
+                  {
+                      "id": 32,
+                      "title": "房产常识",
+                      "secondSubTitleList": null
+                  },
+                  {
+                      "id": 33,
+                      "title": "家装课堂",
+                      "secondSubTitleList": null
+                  }
+              ]
+          },
+          {
+              "categoryId": 60,
+              "title": "风采",
+              "imageUrl": "https://img.wkzf.com/6a3476d97ca94a08b422739f18cdd30b",
+              "firstSubTitleList": null
+          }
+      ] ,
+      "showLocation" : 1 
+  }
+}
+export default apiData ;
